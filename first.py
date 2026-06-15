9c12327e85176c5729f3bc0d988a578ed73436b1
AM BAKSH <ambaksh731@gmail.com>
2026-06-15 18:11:51 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Hello world")
+
+print("i am coder")
+
+print("ldfkfk")
+
+print("My self abdul, i m from bihar, alhamdulillah  i m hafiz,and an mechanical engg.\n now i m switching my depth from mech to ai engg fields")
